7f7597f1e0e15a0294ce29b17f712e4d21230c6f
krychkarol <krych.karol.93@gmail.com>
2022-04-06 18:39:38 +0200

    about section

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+window.onscroll = function () {
+    let aboutSection = isScrolledIntoView(document.getElementById("aboutAnimate"));
+
+    //ABOUT ANIMATION TRIGGER
+    if(aboutSection){
+        about.classList.add('animate');
+        aboutQuote.classList.add('animate');
+        aboutAboutme.classList.add('animate');
+        for(let i = 0; i < aboutHTML.length; i++){
+            aboutHTML[i].classList.add('animate');
+        }
+        }
+};
+
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
+
 //NAVBAR
 document.getElementById("open-menu").addEventListener("click", toggleMenu);
 document.getElementById("close-menu").addEventListener("click", toggleMenu);
@@ -28,11 +51,24 @@ function toggleMenu() {
     }
 }
 
-//HOME ANIMATION
+//HOME ANIMATION DELAY
 
-let x = document.getElementById('animationSlideIn');
+let x = document.getElementById('home-animationSlideIn');
 for(let i = 0; i < x.children.length; i++){
     if(x.children[i].localName === "span"){
         x.children[i].style.animationDelay = ((i * 0.07) + "s")
     }
 }
+
+//ABOUT ANIMATION DELAY
+
+let about = document.getElementById('about-animationLeftToRight');
+for(let i = 0; i < about.children.length; i++){
+    if(about.children[i].localName === "span"){
+        about.children[i].style.animationDelay = ((i * 0.07) + "s");
+    }
+}
+
+let aboutHTML = document.getElementsByClassName('aboutHTML');
+let aboutQuote = document.getElementById('aboutQuote');
+let aboutAboutme = document.getElementById('aboutAboutme');
